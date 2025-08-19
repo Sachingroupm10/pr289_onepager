80ccd570b677e4923ddb46925083abb587d87335
Sachin Saurav <sachin.saurav@groupm.com>
2025-08-19 13:52:09 +0530

    exposed 8080 port

--- app.py
+++ app.py
@@ -143,5 +143,5 @@ if __name__ == '__main__':
   print("Base64 A:", base64_a)
   print("Base64 B:", base64_b)
 
-  port = int(os.environ.get("PORT", 8080))
-  app.run(host='0.0.0.0', port=port, debug=True)
+  #port = int(os.environ.get("PORT", 8080))
+  app.run(host='0.0.0.0', port=8080, debug=True)
